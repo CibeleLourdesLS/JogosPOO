9f8888a4d628137843658ed45821a690c3c1f3be
CibeleLourdesLS <cibelelourdes@gmail.com>
2016-06-09 10:29:38 -0300

    Atualização 1
    Nova Atualização
    Código Comentado

---
+++ Aula1.cpp
@@ -0,0 +1,14 @@
+//Meu primeiuro código em C++
+
+//Biblioteca
+#include<iostream>
+using namespace std;
+
+//Declaração das variaveis
+int main(int agrc, char* agrs[])
+{
+	//Imprimir na tela
+	cout << "Hello Word" << endl;
+
+	return 0;
+}
